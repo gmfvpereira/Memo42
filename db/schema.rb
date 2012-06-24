f139f051e242c4f2b40861618153a153fbdea646
gmfvpereira <gmfvpereira@gmail.com>
2012-06-24 01:07:11 -0300

    added attributes for facebook authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623133034) do
+ActiveRecord::Schema.define(:version => 20120624035653) do
 
   create_table "facts", :force => true do |t|
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "likes"
   end
 
   create_table "users", :force => true do |t|
@@ -32,6 +33,9 @@ ActiveRecord::Schema.define(:version => 20120623133034) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
